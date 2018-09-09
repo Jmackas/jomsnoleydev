7100d026de89f9c8bc1aff930b5e976689516f0d
James Mackay <jamesrossmackay@gmail.com>
2018-09-09 13:24:28 +0800

    Update Arrays.cs

--- James M Lab 1/Examples/Arrays.cs
+++ James M Lab 1/Examples/Arrays.cs
@@ -1,2 +1,13 @@
 // Declaration of an array
 var dieRollOne = new int[7];
+
+// Add the arrays together
+for (int numInc = 1; numInc <= 6; numInc++)
+{
+  outputOne[numInc] = dieRollOne[1] + dieRollTwo[numInc];
+  outputTwo[numInc] = dieRollOne[2] + dieRollTwo[numInc];
+  outputThree[numInc] = dieRollOne[3] + dieRollTwo[numInc];
+  outputFour[numInc] = dieRollOne[4] + dieRollTwo[numInc];
+  outputFive[numInc] = dieRollOne[5] + dieRollTwo[numInc];
+  outputSix[numInc] = dieRollOne[6] + dieRollTwo[numInc];
+}
